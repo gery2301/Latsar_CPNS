50874f80fdb1812256e4038f49546f701f00e3bf
gery2301 <147959524+gery2301@users.noreply.github.com>
2025-11-05 08:40:43 +0800

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -96,14 +96,28 @@ map.on('draw:deleted', function (e) {
 // ===============================
 // LOAD DATA AWAL
 // ===============================
-fetch(GAS_URL)
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(d => {
-      const geom = JSON.parse(d.geometry);
-      const layer = L.geoJSON(geom).addTo(drawnItems);
-      layer.bindPopup(`<b>${d.nama}</b>`);
-      layer.eachLayer(l => l.options.id = d.id); // simpan ID
-    });
-  })
-  .catch(err => console.error(err));
+ fetch("https://script.google.com/macros/s/AKfycbyKBHseSt8bdyO05fUw52Nzs6sGJ18tIkTvl2FfTKz2Ey0TKiW2hxJu4i_z7Ur7-doP/exec")
+      .then(res => res.json())
+      .then(data => {
+        // Misal data = [{nama:"Kantor A", lat:-8.5, lon:119.9}, ...]
+        console.log(data);
+        data.data.forEach(d => {
+           if (!d.geometry) return;
+
+            const type = d.geometry.type;
+            const coords = d.geometry.coordinates;
+          
+            if (type === "Point") {
+              const [lon, lat] = coords;
+              L.marker([lat, lon]).addTo(map)
+                .bindPopup(`<b>${d.nama_sekolah}</b>`);
+            } else if (type === "LineString") {
+              const latlngs = coords.map(([lon, lat]) => [lat, lon]);
+              L.polyline(latlngs, {color:'blue'}).addTo(map);
+            } else if (type === "Polygon") {
+              const latlngs = coords.map(ring => ring.map(([lon, lat]) => [lat, lon]));
+              L.polygon(latlngs, {color:'green'}).addTo(map);
+            }
+          });
+      })
+      .catch(err => console.error(err));

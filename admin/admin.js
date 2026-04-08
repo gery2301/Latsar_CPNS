f0665a027b87312a630ad53b546a5b359661493d
gery2301 <147959524+gery2301@users.noreply.github.com>
2026-04-08 22:19:37 +0800

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -45,13 +45,11 @@ map.on(L.Draw.Event.CREATED, function (e) {
   if (!nama) return;
 
   const payload = {
-  nama_sekolah: nama,
-  status: "NEGERI", // sementara
-  geometry: geom     // JANGAN stringify di sini
-};
+    action: "create",
+    nama: nama,
+    geometry: JSON.stringify(geom)
+  };
 
-  console.log(payload);
-  
   fetch(GAS_URL, {
     method: "POST",
     body: JSON.stringify(payload)
